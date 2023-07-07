e5a981583d6265fc466c850d152a30b28d5c4633
gkz <giorgadzekonstantin@gmail.com>
2023-07-07 15:14:12 +0400

    Added package

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,6 +6,7 @@ public class StatisticsServiceTest {
 
     @Test
     void findMax() {
+
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
